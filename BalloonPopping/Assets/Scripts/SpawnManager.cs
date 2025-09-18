2aede306c9e0aa910c0974521f16b89efcb062d4
Tyler0512 <tyler.s291@stu.nebo.edu>
2025-09-18 14:15:20 -0600

    balloon spawner

---
+++ BalloonPopping/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnManager : MonoBehaviour
+{
+
+
+    public GameObject[] balloonPrefabs; // Array of balloon prefabs
+    public float startDelay = 0.5f; // Delay before the first balloon spawns
+    public float spawnInterval = 1.5f; // Interval between balloon spawns
+    public float xRange = 10.0f; // Range for spawning balloons on the X axis
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBalloon", startDelay, spawnInterval);
+    }
+
+    void SpawnRandomBalloon()
+    {
+        
+        // Generate a random position within the specified xRange
+        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange), transform.position.y, transform.position.z);
+
+        // Generate a random index to select a balloon prefab
+        int balloonIndex = Random.Range(0, balloonPrefabs.Length);
+
+        // Instantiate the selected balloon prefab at the generated position with no rotation
+        Instantiate(balloonPrefabs[balloonIndex], spawnPos, balloonPrefabs[balloonIndex].transform.rotation);
+    }
+}
